5a520a66deb8797733959cef541b1187e076360d
SHRIRAM <nshriram1326@gmail.com>
2024-08-21 17:11:38 +0530

    chore: Add .venv to .gitignore and configure generative AI model

---
+++ api_test.py
@@ -0,0 +1,8 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.environ["API_KEY"])
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+prompt = ""
+response = model.generate_content(prompt, stream=True)
